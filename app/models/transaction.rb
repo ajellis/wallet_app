9b74bca73829543b42460f612ce3585b8250cb6f
Anthony Ellis <ajellis1989@gmail.com>
2015-10-01 03:47:18 -0400

    Adds methods for tracking withdrawn money and well as deposited money

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,17 @@ class Transaction < ActiveRecord::Base
   def total
     self.count
   end
+
+  def self.withdraw
+    self.all.select {|t| t.transaction_type == "Withdraw Money"}
+  end
+
+  def self.deposits
+    self.all.select {|t| t.transaction_type == "Deposit Money"}
+  end
+
+  def self.most_spent
+    withdraw.map{|t| t.amount}.sort.last
+  end
+
 end
