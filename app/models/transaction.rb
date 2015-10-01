6cafa7fdb6f12c386d7af14b07c34d55070acf8d
Anthony Ellis <ajellis1989@gmail.com>
2015-10-01 03:57:58 -0400

    Adds a fw more things to the root/dashboard

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,15 +2,15 @@ class Transaction < ActiveRecord::Base
 
   self.inheritance_column = "inheritance_type"
 
-  def total
+  def self.total_transactions
     self.count
   end
 
-  def self.withdraw
+  def self.number_of_withdraws
     self.all.select {|t| t.transaction_type == "Withdraw Money"}
   end
 
-  def self.deposits
+  def self.number_of_deposits
     self.all.select {|t| t.transaction_type == "Deposit Money"}
   end
 
